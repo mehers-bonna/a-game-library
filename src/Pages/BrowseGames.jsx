fcd619a4fc3f420a766a95d850e38d4bf165ccd8
mehers-bonna <mehersbonna17131713@gmail.com>
2025-10-22 11:35:29 -0600

    added browsegames page and other changes

--- src/Pages/BrowseGames.jsx
+++ src/Pages/BrowseGames.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import GameCard from './GameCard';
 
 const BrowseGames = () => {
+   const [games, setGames] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('/games.json')
+      .then(res => res.json())
+      .then(data => {
+        setGames(data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
     return (
-        <div>
-            <h1>browse</h1>
-        </div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-blue-500"></div>
+      </div>
     );
+  }
+
+  return (
+    <div>
+        <h1 className='text-4xl font-bold text-center py-10'>All Gaming Apps</h1>
+        <div className="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
+      {games.map(game => (
+        <GameCard key={game.id} game={game} />
+      ))}
+    </div>
+    </div>
+  );
 };
 
 export default BrowseGames;
